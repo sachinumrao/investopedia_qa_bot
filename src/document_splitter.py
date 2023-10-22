d7c8e905e7f0533be83cbe9ae7a2f91b156898b4
sachinumrao <sachin.umrao.1512@gmail.com>
2023-10-22 09:56:54 +0530

    first version of document splitter

---
+++ src/document_splitter.py
@@ -0,0 +1,108 @@
+"""
+This code reads from the dump of investopedia pages and creates chunks of data.
+chunks can be a jsonl file with chunks containing following attributes:
+{
+    doc_id: doc id of investopedia page
+    chunk_id: denoting i th chunk from the given doc id
+    content: text chunk from document
+}
+"""
+
+from pathlib import Path
+import os, time, json
+import jsonlines
+from nltk.tokenize import sent_tokenize, word_tokenize
+from tqdm.auto import tqdm
+import pandas as pd
+
+RAW_FILES_PATH = Path("~/Data/Investopedia/investopedia.csv")
+OUTPUT_FILEPATH = Path("~/Data/Investopedia/investopedia_chunks.jsonl")
+
+DEBUG = os.environ.get("DEBUG", 0)
+
+
+def read_data():
+    data_df = pd.read_csv(RAW_FILES_PATH)
+    if DEBUG == "1":
+        print("Data: ")
+        print(data_df.head())
+        print("Shape of data: ", data_df.shape)
+        print("Columns in data: ", data_df.columns)
+
+    return data_df
+
+
+def chunkify(data_df: pd.DataFrame):
+    print("Chunking data...")
+    for _, row in tqdm(data_df.iterrows(), total=data_df.shape[0]):
+        text = row["Text"]
+        doc_id = row["DocID"]
+        # get list of sentecnes
+        sents = sent_tokenize(text)
+
+        # aggregate sentecnes into chunks of size ~350
+        counter = 0
+        chunks = []
+        len_counter = 0
+        curr_sents = []
+        max_word_count = 350
+
+        for s in sents:
+            n = len(word_tokenize(s))
+            if len_counter + n < max_word_count:
+                curr_sents.append(s)
+                len_counter += n
+
+            else:
+                # append chunk
+                chunks.append(
+                    {
+                        "doc_id": doc_id,
+                        "chunk_id": counter,
+                        "chunk_content": " ".join(curr_sents),
+                    }
+                )
+                counter += 1
+
+                # reset local counters
+                curr_sents = []
+                len_counter = 0
+
+                # add current sentecne to new chunk segment
+                curr_sents.append(s)
+                len_counter == n
+
+    return chunks
+
+
+def main():
+    # read data
+    data_df = read_data()
+
+    # clean data
+    # print some samples to assess cleaning requirements
+    if DEBUG == "2":
+        tmp_df = data_df.sample(20)
+        for _, row in tmp_df.iterrows():
+            print("----------Sample----------")
+            print(row["Text"])
+            print("\n\n")
+
+    # chunkfiy data
+    chunks = chunkify(data_df)
+
+    # save chunks
+    with jsonlines.open("investopedia_chunks.jsonl", mode="w") as writer:
+        for c in chunks:
+            writer.write(c)
+
+
+if __name__ == "__main__":
+    main()
+
+
+## TODO
+# fix output file save method
+# add support for command line args for input and output files, overlap window
+# experiment with spacy for sentecne tokenization
+# support overlap window in chunks
